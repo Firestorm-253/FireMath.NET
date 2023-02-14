074ce6f5f340201cab744bd8a53bf1489652417c
Firestorm-253 <laurin.zehnder@hotmail.com>
2023-02-14 19:29:36 +0100

    Trigonometry

---
+++ src/Math.NET/Math.NET/Trigonometry.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+
+namespace Math.NET;
+
+public static class Trigonometry
+{
+    public static double ToRadian(this double degree)
+    {
+        return degree / (180.0 / System.Math.PI);
+    }
+    public static double AngleRadian(Vector2 vector)
+    {
+        return AngleRadian(vector.X, vector.Y);
+    }
+    public static double AngleRadian(double x, double y)
+    {
+        return Base.Atan2(y, x);
+    }
+}
