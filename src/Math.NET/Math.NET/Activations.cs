0ed0ce15c8913becd1d41351767f56cd84559150
Firestorm-253 <laurin.zehnder@hotmail.com>
2023-02-14 19:27:22 +0100

    Activations

---
+++ src/Math.NET/Math.NET/Activations.cs
@@ -0,0 +1,12 @@
+﻿namespace Math.NET.Activations;
+
+public static class Activations
+{
+    public static double TanH(this double value) => System.Math.Tanh(value);
+
+    public static double Sigmoid(this double value) => 1 / (1 + Base.Exp(-value));
+
+    public static double LeakyRelu(this double value, double alpha = 0.3) => (value >= 0) ? value : (value * alpha);
+
+    public static double Softplus(this double value, double alpha = 0.3) => (1 + Base.Exp(value)).Log();
+}
